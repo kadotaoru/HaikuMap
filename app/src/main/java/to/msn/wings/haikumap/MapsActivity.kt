1484deb0b0ae5c001c4097bfaeb64c6c719f10fe
kadotaoru <daiyaogata898@gmail.com>
2020-12-13 19:28:22 +0900

    Marker Dragable

--- app/src/main/java/to/msn/wings/haikumap/MapsActivity.kt
+++ app/src/main/java/to/msn/wings/haikumap/MapsActivity.kt
@@ -14,40 +14,58 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 //import java.util.jar.Manifest
 import android.Manifest
+import android.os.Build
+import android.system.Os.remove
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.Marker
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMapLongClickListener,
+    GoogleMap.OnMarkerClickListener {
 
     private lateinit var mMap: GoogleMap
     private val MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION = 1
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
-                .findFragmentById(R.id.map) as SupportMapFragment
+            .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
-    }
-    
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
 
-        val sydney = LatLng(-34.0, 151.0)
-        
-        checkPermission()
     }
-    
-    private fun checkPermission(){
-        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-            == PackageManager.PERMISSION_GRANTED) {
+
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        //lateinitの初期化（参照するまえに宣言しないとエラーになる）
+        mMap = googleMap
+        //長押しイベントの取得
+        mMap.setOnMapLongClickListener(this)
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            checkPermission()
+        } else {
+            mMap.isMyLocationEnabled = true
+        }
+    }
+
+    private fun checkPermission() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED
+        ) {
             myLocationEnable()
         } else {
             requestLocationPermission()
         }
     }
 
-    private fun requestLocationPermission(){
-        if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+    private fun requestLocationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        ) {
             //許可を求め、拒否された場合
             ActivityCompat.requestPermissions(
                 this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
@@ -55,35 +73,58 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             )
         } else {
             // まだ許可を求めていない
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION)
+            ActivityCompat.requestPermissions(
+                this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION
+            )
         }
     }
-    
-        override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-            when (requestCode) {
-                MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION -> {
-                    if (permissions.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                        //許可された
-                        myLocationEnable()
-                    } else {
-                        showToast("現在位置は表示できません")
-                    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION -> {
+                if (permissions.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //許可された
+                    myLocationEnable()
+                } else {
+                    showToast("現在位置は表示できません")
                 }
             }
         }
+    }
 
-        private fun myLocationEnable(){
-            //赤い波線でエラーが表示されてしまうので
-            if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED){
-                mMap.isMyLocationEnabled = true
-            }
-        }
-
-        private fun showToast(msg: String){
-            val toast = Toast.makeText(this, msg, Toast.LENGTH_LONG)
-            toast.show()
+    private fun myLocationEnable() {
+        //赤い波線でエラーが表示されてしまうので
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            mMap.isMyLocationEnabled = true
         }
     }
+
+    private fun showToast(msg: String) {
+        val toast = Toast.makeText(this, msg, Toast.LENGTH_LONG)
+        toast.show()
+    }
+
+    override fun onMapLongClick(point: LatLng) {
+        //draggableマーカーをドラッグで動かせるようにする。Bitmap~でマーカーの色のカスタマイズ
+        var marker = MarkerOptions().position(point).title("目的地").draggable(true).icon(
+            BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)
+        ).snippet("Yay!!")
+
+        mMap!!.addMarker(marker)
+    }
+
+    //マーカーをタップしたら削除する（未達成）
+    override fun onMarkerClick(marker: Marker): Boolean {
+        marker.remove()
+        return false
+    }
+}
+
